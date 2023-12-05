01f88cf4d1f60432c00306c805e334f9ca1365cb
gaazoo <gaazoo@yandex.ru>
2023-12-06 00:31:25 +0700

    Coding

---
+++ LinkedList.java
@@ -0,0 +1,54 @@
+public class LinkedList<T> {
+    private Node head;
+    private int size;
+
+    private class Node {
+        T data;
+        Node next;
+
+        Node(T data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public void insertAtBeginning(T data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void insertAtEnd(T data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public T getElementAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,24 @@
+/*Реализовать свой LinkedList с методами вставки в начало, конец списка, 
+получения размера списка и получения
+элемента по индексу. (можете выбрать список однонаправленный или двунаправленный, 
+по желанию)
+*/
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.insertAtBeginning("1");
+        list.insertAtEnd("2");
+        list.insertAtEnd("3");
+        list.insertAtEnd("4");
+        list.insertAtBeginning("5");
+
+        System.out.println("Размер списка: " + list.getSize() + " элементов");
+
+        System.out.println("Элемент под индексом 0: " + list.getElementAtIndex(0));
+        System.out.println("Элемент под индексом 1: " + list.getElementAtIndex(1));
+        System.out.println("Элемент под индексом 2: " + list.getElementAtIndex(2));
+        System.out.println("Элемент под индексом 3: " + list.getElementAtIndex(3));
+        System.out.println("Элемент под индексом 4: " + list.getElementAtIndex(4));
+    }
+}
